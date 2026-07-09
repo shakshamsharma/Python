1f16643c649867ac13cd6ebd759f4e61b9214e23
shakshamsharma <sakshamsharmaa15@gmail.com>
2026-07-09 17:59:21 +0530

    more Practice

---
+++ inheritance.py
@@ -0,0 +1,81 @@
+"""class Animal:
+    def eat(self):
+        print("Eating")
+    def sleep(self):
+        print("Sleeping")
+dog1 = Animal()
+dog1.eat()
+dog1.sleep()
+
+
+class Animal:
+    def eat(self):
+        print("Eating")
+
+class Dog(Animal):
+    def bark(self):
+        print("Whoof Whoof")
+        
+dog1 = Dog()
+dog1.eat()
+dog1.bark()
+
+class Vehicle:
+    def start(self):
+        print("Vehicle Started")
+class Car(Vehicle):
+    pass
+Car1 = Car()
+Car1.start()
+
+
+class Company:
+    company = 'Microsoft'
+    def developer(self):
+        pass
+developer1 = Company()
+print(developer1.company)
+
+"""
+
+#Practice
+"""
+class Person:
+    def walk(self):
+        print("walking")
+class student(Person):
+    pass
+s1 = student()
+s1.walk()
+
+
+class Bird:
+    def fly(self):
+        print("flying")
+class Perrot(Bird):
+    pass
+bird1 = Perrot()
+bird1.fly()
+
+
+class Device:
+    def power_on(self):
+        print("I am Saksham")
+        
+class Laptop(Device):
+    def code(self):
+        print("Saksham The Great")
+lap1 = Laptop()
+lap1.power_on()
+lap1.code()
+"""
+
+class Vehicle:
+    def start(self):
+        print("Vehicle Started")
+class Bike(Vehicle):
+    def ride(self):
+        print("Bike is Riding")
+bike1 = Bike()
+bike1.start()
+bike1.ride()
